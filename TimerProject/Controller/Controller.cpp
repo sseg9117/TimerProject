2189bb5563e91bb890948d3ddf51ed7675bdcb60
Sheradon Segota <31513874+sseg9117@users.noreply.github.com>
2018-02-01 09:18:13 -0700

    did timer class stuff and fixed setup to look cleaner forgot to commit during all of it :/.

---
+++ TimerProject/Controller/Controller.cpp
@@ -0,0 +1,30 @@
+//
+//  Controller.cpp
+//  TimerProject
+//
+//  Created by Segota, Sheradon on 2/1/18.
+//  Copyright © 2018 Segota, Sheradon. All rights reserved.
+//
+
+#include "Controller.hpp"
+using namespace std;
+
+void Controller :: start()
+{
+    cout << "Testing the timeclass" << endl;
+    Timer codeTimer;
+    codeTimer.startTimer();
+    cout << "print something to the screen!" << endl;
+    codeTimer.stopTimer();
+    codeTimer.displayInformation();
+    
+    codeTimer.resetTimer();
+    
+    codeTimer.startTimer();
+    for(int i = 0; i < 100; i++)
+    {
+        cout << "index is at " << i << "\t";
+    }
+    codeTimer.stopTimer();
+    codeTimer.displayInformation();
+}
---
+++ TimerProject/Controller/Controller.hpp
@@ -0,0 +1,20 @@
+//
+//  Controller.hpp
+//  TimerProject
+//
+//  Created by Segota, Sheradon on 2/1/18.
+//  Copyright © 2018 Segota, Sheradon. All rights reserved.
+//
+
+#ifndef Controller_hpp
+#define Controller_hpp
+#include "Tools/Timer.hpp"
+#include <iostream>
+
+class Controller
+{
+public:
+    void start();
+};
+
+#endif /* Controller_hpp */
