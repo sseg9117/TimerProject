c5acf48ee216bfc91291accb405e1ff1c811a473
Sheradon Segota <31513874+sseg9117@users.noreply.github.com>
2018-02-02 09:19:23 -0700

    added all my clases.

--- TimerProject/Model/Structures/Array.hpp
+++ TimerProject/Model/Structures/Array.hpp
@@ -37,5 +37,83 @@ public:
     int getSize() const;
     Type getFromIndex(int index);
     void setAtIndex(int index, Type data);
+};
+template <class Type>
+Array<Type> :: Array(int size)
+{
+    assert(size > 0);
+    this->size = size;
+    
+    internalArray = new Type[size];
+}
+template <class Type>
+Array<Type> :: Array(const Array<Type> & toCopy)
+{
+    this->size = toCopy.getSize();
+    
+    //Build Data Structure
+    internalArray = new Type[size];
+    
+    for(int index = 0; index < size; index++)
+    {
+        internalArray[index] = toCopy[index];
+    }
+}
+template <class Type>
+Array<Type> :: ~Array()
+{
+    delete [] internalArray;
+}
+template <class Type>
+Array<Type> & Array<Type> :: operator = (const Array<Type> & toAssign)
+{
+    if (&toAssign != this)
+    {
+        if (size != toAssign.getSize())
+        {
+            delete [] internalArray;
+            size = toAssign.getSize();
+            internalArray = new Type [size];
+        }
+        
+        for (int index = 0; index < size; index++)
+        {
+            internalArray[index] = toAssign[index];
+        }
+    }
+    return *this;
+}
+template <class Type>
+Type & Array <Type> :: operator [] (int index)
+{
+    assert(index >= 0 && index < size);
+    return internalArray[index];
+}
+
+template <class Type>
+Type Array<Type> :: operator [] (int index) const
+{
+    assert(index >= 0 && index < size);
+    return internalArray[index];
+}
+template <class Type>
+int Array<Type> :: getSize() const
+{
+    return size;
+}
+template <class Type>
+Type Array<Type> :: getFromIndex(int index)
+{
+    assert(index >= && Index int index)
+    
+    Type value = internalArray[index];
+    
+    return value;
+}
+template <class Type>
+void Array<type> :: setAtIndex(int pos, Type item)
+{
+    assert(pos >= 0 && pos < size);
+    internalArray[pos] = item;
 }
 #endif /* Array_h */
